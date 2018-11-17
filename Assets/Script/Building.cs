47c7af013e08eb4eee113e5a26ee5f4027298c53
Aurélia <aurelia96@free.fr>
2018-11-17 01:05:26 +0100

    Temple and Field prefabs

---
+++ Assets/Script/Building.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Building : MonoBehaviour {
+
+    [SerializeField]
+    private int deltaHabitants;
+
+    [SerializeField]
+    private int deltaFood;
+
+    [SerializeField]
+    private int deltaSouls;
+
+
+}
