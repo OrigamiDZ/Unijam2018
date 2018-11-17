b841f4082f574fe3236dceb4b12b88fc92f78e35
Unknown <shems-dean.bourass@telecom-sudparis.eu>
2018-11-17 10:03:26 +0100

    Add InputManager + create scene

---
+++ Assets/Script/InputManager_Shems.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager_Shems : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown("escape"))
+        {
+            Debug.Log("escape pressed");
+        }
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            Debug.Log(hit.transform.name);
+        }
+    }
+}
