6a1b2c7022a7595e5367cfed36a95f0d3dc89b75
Unknown <shems-dean.bourass@telecom-sudparis.eu>
2018-11-18 02:01:24 +0100

    Modif InputManagerGodCouncil

---
+++ Assets/Script/InTheGodsRoomManager_Shems.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InTheGodsRoomManager_Shems : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log(InputManagerGodCouncil_Shems.divinity);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
